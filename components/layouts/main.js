477c3acdf549c51c79b206179ba8224b4eb57143
Jun <69883534+asparagus0161@users.noreply.github.com>
2024-10-17 18:46:40 +1300

    Updated gitignore and added few react components

---
+++ components/layouts/main.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+import { Box, Container } from "@chakra-ui/react"
+
+
+const Main = ({children, router}) => {
+    return (
+        <Box as="main" pb={8}>
+            <Head>
+                <meta name="viewport" content="width-=device-wdith, initial-scale=1"/>
+                <title>Jun Yi Ng - Homepage</title>
+            </Head>
+            <Container maxW="container.md" pt={14}>
+                {children}
+            </Container>
+        </Box>
+    )
+}
+
+export default Main

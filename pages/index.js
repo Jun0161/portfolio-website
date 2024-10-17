6bda12823b7405c042e46b040dff0693f9374f3d
Jun <69883534+asparagus0161@users.noreply.github.com>
2024-10-18 10:28:15 +1300

    Main completed, added animation to theme swap

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
-import { Container, Box, Heading, Image, useColorModeValue} from "@chakra-ui/react";
-import Section from "../components/section"
+import {
+  Link,
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+  Button,
+  List,
+  ListItem
+} from '@chakra-ui/react'
+import NextLink from 'next/link'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/bio'
+import { IoLogoGithub } from 'react-icons/io5'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")} p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I&apos;m a Computer Science Student based in New Zealand!
       </Box>
 
-      <Box display={{ md: "flex" }}>
+      <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
             Jun Yi Ng
@@ -21,15 +42,88 @@ const Page = () => {
           ml={{ md: 6 }}
           align="center"
         >
-          <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" display="inline-block" borderRadius="full" src="/69883534.jfif" alt="Profile Image"/>
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/69883534.jfif"
+            alt="Profile Image"
+          />
         </Box>
       </Box>
       <Section delay={0.1}>
-        <Heading as ="h3" variant="section-title">
-            Work
+        <Heading as="h3" variant="section-title">
+          Work
         </Heading>
+        <Paragraph>
+          Jun is a computer science student and full-stack developer with a
+          passion for automating daily tasks through innovative scripts.
+          Enthusiastic about tech, Jun constantly explores new technologies and
+          trends in the industry. With a deep love for solving complex problems
+          and building highly intricate products, Jun enjoys the challenge of
+          bringing ambitious ideas to life. Equipped with strong communication
+          skills, Jun thrives in both collaborative and individual projects,
+          always seeking to push boundaries and create impactful digital
+          solutions.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/projects">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My Projects
+            </Button>
+          </NextLink>
+        </Box>
       </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2003</BioYear>
+          Born in Kedah, Malaysia.
+        </BioSection>
+        <BioSection>
+          <BioYear>2024</BioYear>
+          Graduated with a Bachelor of Science, achieving a GPA of 5.4 (B-A
+          average).
+        </BioSection>
+        <BioSection>
+          <BioYear>2025-2026</BioYear>
+          Expected to begin a Master’s program in Analytics.
+        </BioSection>
+      </Section>
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+        Problem-solving, automating repetitive, finding creative solutions, exploring the architecture of game infrastructure
+        </Paragraph>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          On the web
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/asparagus0161" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoGithub />}
+              >
+                @asparagus0161
+              </Button>
+            </Link>
+          </ListItem>
+          </List>
+          </Section>
     </Container>
-  );
-};
-export default Page;
+  )
+}
+
+export default Page

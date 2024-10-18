2a38d0036ce548e4d872288f4ba784f7e08719c2
Jun <69883534+asparagus0161@users.noreply.github.com>
2024-10-18 16:17:48 +1300

    Updated Readme, Added title to every page

--- pages/404.js
+++ pages/404.js
@@ -7,19 +7,22 @@ import {
   Button,
   Text
 } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
 
 const NotFound = () => {
   return (
-    <Container>
-      <Heading as="h1">Not Found</Heading>
-      <Text>The page you&apos;re looking for was not found.</Text>
-      <Divider my={6} />
-      <Box my={6} align="center">
-        <NextLink href="/">
-          <Button colorScheme="teal">Return to home</Button>
-        </NextLink>
-      </Box>
-    </Container>
+    <Layout title="404">
+      <Container>
+        <Heading as="h1">Not Found</Heading>
+        <Text>The page you&apos;re looking for was not found.</Text>
+        <Divider my={6} />
+        <Box my={6} align="center">
+          <NextLink href="/">
+            <Button colorScheme="teal">Return to home</Button>
+          </NextLink>
+        </Box>
+      </Container>
+    </Layout>
   )
 }
 
